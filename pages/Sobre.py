80431818a62f9395c834bfced150cd16108e0264
luizlopes65 <130614375+luizlopes65@users.noreply.github.com>
2024-02-11 12:51:05 -0300

    Delete pages/Sobre.py

--- pages/Sobre.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.markdown("<h1 style='text-align: left;'>Sobre Mim</h1>", unsafe_allow_html=True)
-
-
-st.write('Meu nome é Luiz Eduardo Foschiera Couto Lopes. Nasci em Brasília, DF, Brasil, porém vivi grande parte da minha vida em Goiânia, GO, e hoje resido em Campinas,SP, onde estudo Matemática Aplicada e Computacional - Universidade Estadual de Campinas, Unicamp.')
-st.write('Sempre fui interessado em ciência e era entusiasta de olimpíadas do conhecimento. Possuo uma medalhas de ouro e uma de prata na Olimpiada Nacional de Ciências e uma de ouro na Olimpíada Brasileira de Astronomia, porém á participei da Olimpíada brasileira de Física, Olimpíada Brasileira de Bioloquia, Olimpíada Brasileira de Química e Olimpíada Nacional de História do Brasil.')
-st.write('Além de ciência, gosto de me desnvolver no esporte, buscando saúde física e clareza mental. Possuo faixa preta em judô, 13 anos de treino de capoeira e alguns anos de karatê e jiu-jitsu. Mas não somente artes marciais, sou adepto do hábito da corrida e da musculação, esportes que me acompanham diariamente.')
-st.write('No futuro, me vejo tanto incorporado ao mercado de trabalho, quanto ao meio acadêmico-científico, em torno da área de machine learning, aplicação de IA e desenvolvimento e construção de algoritmos.')
