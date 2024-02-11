77b167d5fe414ccf9b10b1ce73ba979217ff57a8
luizlopes65 <130614375+luizlopes65@users.noreply.github.com>
2024-02-11 12:53:07 -0300

    Add files via upload

---
+++ Experiências.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>Experiências</h1>", unsafe_allow_html=True)
+
+with st.expander('Programação e Pesquisa'):
+    st.write('CNPEM e projetos')
+
+with st.expander('Liderança e Trabalho em Equipe'):
+    st.write('Baateria e Iris')
